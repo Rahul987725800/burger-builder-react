306856cdb2bf81180030d7724f4289bed3b0d701
Rahul Gupta <guptarahul70322@gmail.com>
2020-08-14 15:07:37 +0530

    burger ingredients complete

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aux from '../../hoc/_Aux/_Aux'
+import Burger from '../../components/Burger/Burger';
+class BurgerBuilder extends React.Component {
+    render() {
+        return(
+            <Aux>
+                <Burger />
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+export default BurgerBuilder;   
